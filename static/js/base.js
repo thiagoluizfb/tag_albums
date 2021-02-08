d2e1aa67c5d74281c3ac2897fcab7b221ed45f3d
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-08 00:02:11 +0000

    Add function to remove thumbnails in upload page

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
-  var images = [];
-  var firstTag;
+var images = [];
+var firstTag;
 
 function addtags() {
     allTags = $("#tag-all-files").val();
@@ -16,16 +16,20 @@ function addtags() {
     $("#tag-all-files-btn").html("Update shared tags");
 };
 
-  var loadFile = function(event) {
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
+
+var loadFile = function(event) {
     $(".upload-photo-btn").html("Add more");
     $(".image-container").css("display", "none");
     $(".addtags-wrapper").css("display", "block");
     var numFiles = $("#upload-photo")[0].files.length;
     for (i = 0; i < numFiles; i++) {
-      imageName = event.target.files[i].name;
-      console.log(event.target.files[i]);
-      //console.log(images.indexOf(image_name));
-      if (images.indexOf(imageName)<0){
+        imageName = event.target.files[i].name;
+        console.log(event.target.files[i]);
+        //console.log(images.indexOf(image_name));
+        if (images.indexOf(imageName)<0){
         images.push(imageName);
         var count = $("#frame").children().length/2;
         imageName = event.target.files[i].name;
@@ -37,11 +41,16 @@ function addtags() {
         idName = `output${count}`;
         $("#frame").append(`
         <div class="col-12 col-md-6 col-lg-3 text-center">
-        	<div class="frame-wrapper">
-            	<img id="${idName}" class="upload-thumbnail"/>
+            <div class="frame-wrapper position-relative">
+                <img id="${idName}" class="upload-thumbnail" onmouseenter="showdelete(this)" onmouseleave="hidedelete(this)"/>
+                <span class="displaynone btn delete-thumbnail">
+                    <strong>
+                        <i class="far fa-times-circle" onclick="deleteimg(this)" data-toggle="tooltip" data-placement="top" title="Remove this file"></i>
+                    </strong>
+                </span>
             </div>
-        	<div class="text-left">
-            	<input type="text" class="tag-slot" placeholder="@tag this file">
+            <div class="text-left position-relative">
+                <input type="text" class="tag-slot" placeholder="@tag this file">
                 <div class="tag-all-slot"></div>
             </div>
         </div>`);
@@ -49,6 +58,18 @@ function addtags() {
         var image = document.getElementById(idName);
         image.src = URL.createObjectURL(event.target.files[i]);
         var count = $("#frame").children().length/2;
-      }
+        }
     }
-  };
+};
+
+function showdelete(btn){
+    $(btn).siblings().removeClass("displaynone");
+}
+
+function hidedelete(btn){
+    $(btn).siblings().addClass("displaynone");
+}
+
+function deleteimg(btn){
+    $(btn).parent().parent().parent().parent().remove();
+}
