3681028957e5d3b66b7e0125ebf6d36c08d90c6d
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-10 23:48:18 +0000

    Add condition to call photos and its tags in uploaded_photos.html

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Photos
+from .models import Photos, Tags
 
 
 def upload(request):
@@ -12,9 +12,12 @@ def uploaded_photos(request):
     """ A view to return the photos page"""
 
     uploaded_photos = Photos.objects.all()
+    tags = Tags.objects.all()
+
 
     context = {
         'uploaded_photos': uploaded_photos,
+        'tags': tags,
     }
 
     return render(request, "uploaded_photos.html", context)
