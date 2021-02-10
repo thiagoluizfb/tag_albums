f4b84d3b038201b2333df2c4adf07c94e712fcd2
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-10 19:35:02 +0000

    Add photos models for photos and tags tables

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Photos, Tags
 
-# Register your models here.
+
+class PhotosAdmin(admin.ModelAdmin):
+    list_display = (
+        'owner',
+        'upload_date',
+        'image',
+    )
+
+
+class TagsAdmin(admin.ModelAdmin):
+    list_display = (
+        'tag_name',
+    )
+
+
+admin.site.register(Photos, PhotosAdmin)
+admin.site.register(Tags, TagsAdmin)
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Photos(models.Model):
+    owner = models.CharField(max_length=254, null=True, blank=True)
+    upload_date = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.owner
+
+
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=100, null=True, blank=True)
+    tag_photos = models.ManyToManyField(Photos)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.tag_name
