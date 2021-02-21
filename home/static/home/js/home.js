fa18c7c0a4bf2a034a6784aa631d53d384d41e86
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-21 17:06:33 +0000

    Create static folder in home and photos app and link related templates. Refactor tamplates, css and js files

---
+++ home/static/home/js/home.js
@@ -0,0 +1,21 @@
+var indexTag = 1
+
+$(document).ready(function() {
+    $('#logo-text').focus();
+});
+
+function logoText() {
+    $('#logo-text').focus();
+    $(".logo-anime").css("font-size", "50px")
+    text = $("#logo-text").val();
+    if (event.keyCode == 13){
+        if (indexTag == 4){indexTag = 1}
+        $(`.index-tag-${indexTag}`).html(`#${text}`);
+        indexTag ++;
+        text = "";
+        $('#logo-text').val(text);
+        $(".logo-anime").html(`@`);
+    }else{
+        $(".logo-anime").html(`@${text}`);
+    }
+};
---
+++ photos/static/photos/js/photos.js
@@ -0,0 +1,24 @@
+var images = []
+
+$('.carousel').carousel()
+
+function loadFile(event) {
+    $(".image-container").addClass("hidden");
+    $(".upload-photo").removeClass("hidden");
+    imageName = event.target.files[0].name;
+    images.push(imageName);
+    $(".frame-wrapper").html(`
+        <img id="img-upload" class="thumbnail"/>`);
+    image = document.getElementById("img-upload");
+    image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function showDelete(btn){
+    $(btn).siblings().removeClass("hidden");
+    return;
+}
+
+function hideDelete(btn){
+    $(btn).siblings().addClass("hidden");
+    return;
+}
