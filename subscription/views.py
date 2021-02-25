031481836d5993b9671e5419540cd6796879ace9
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-25 00:14:37 +0000

    Set up stripe payment Part-I. Define basic css and js configuration. Set public and secret key

--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import UserProfile
+from django.shortcuts import render
+from django.conf import settings
+import stripe
 
 
 def subscribe(request):
     """ A view to view user profile """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     profile = ""
     # get_object_or_404(UserProfile, user=request.user)
@@ -15,8 +18,18 @@ def subscribe(request):
     # else:
     #     form = UserProfileForm(instance=profile)
 
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount="200",
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     context = {
         'profile': profile,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, "subscription/subscribe.html", context)
