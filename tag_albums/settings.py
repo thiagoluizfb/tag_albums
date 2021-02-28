135d09520e378cc854a4771a7df29db3c633b2b6
thiagoluizfb <thiagoluizfb@gmail.com>
2021-02-28 01:58:45 +0000

    Removed secret key and set Debug

--- tag_albums/settings.py
+++ tag_albums/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xw()z#3!3=4z68s7oif9sdqhwj)vzre+2gk4n^u#o-ilqpca*r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tag-albums.herokuapp.com', 'localhost']
 
