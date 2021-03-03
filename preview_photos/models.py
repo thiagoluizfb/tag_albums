e69f58c0a1071382fde0c46d1631683d280f2946
thiagoluizfb <thiagoluizfb@gmail.com>
2021-03-03 15:59:35 +0000

    Change folder to save preview photos, migrate items to environ  database

--- preview_photos/models.py
+++ preview_photos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class PhotosPreview(models.Model):
-    image = models.FileField(null=False, blank=False)
+    image = models.FileField(upload_to='preview', null=False, blank=False)
 
     def __int__(self):
         return self.id
